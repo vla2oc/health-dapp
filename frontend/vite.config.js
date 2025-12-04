be8d88e2e25a4c5a054ec0f950f0a0df1ab3ab25
vla2oc <kurochka265@gmail.com>
2025-12-04 12:31:52 +0100

    MVP

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/health-dapp",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
